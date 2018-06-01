ca4d69a1dcccff1e1470b2d2c5ae1ce8cd91d4cf
Carlo Bilbao <cbilbao88@gmail.com>
2018-06-01 10:03:04 -0700

    feat: use axios to make a network call to dummy api

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,16 +7,55 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state = {
+        post: [],
+        selectedPostId: null
+    }
+
+    //component did mount is the best place to perform network request
+    componentDidMount() {
+        //async
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+
+                //pagination (get only a first 4 items in array)
+                const cutPosts = response.data.slice(0, 4);
+                
+                //update all post object by adding an author
+                const updatedPost = cutPosts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Carlo'
+                    }
+                });
+
+                this.setState({
+                    post: updatedPost //extract data from res and place it in state
+                });
+            }); //promise for async response
+    }
+
+    postSelectorHandler = (id) => {
+        this.setState({selectedPostId : id})
+    }
+
     render () {
+        const post = this.state.post.map(post => {
+            return <Post 
+            key={post.id} 
+            title={post.title} 
+            author={post.author}
+            clicked={() => this.postSelectorHandler(post.id)}/>
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {post}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
