f4abc654d93163a1d5da7d7d68674875fee2ce47
Carlo Bilbao <cbilbao88@gmail.com>
2018-06-01 14:20:01 -0700

    feat: add full post preview

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
+    state = {
+        loadedPost: null
+    }
 
+    componentDidUpdate() {
+
+        if(this.props.id) {
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                .then( response => {
+                    this.setState({loadedPost:response.data});
+                });
+            }
+        }
+
+    }
+
+    render () {
+        //default line
         let post = <p style={{textAlign:'center'}}>Please select a Post!</p>;
 
+        //loading line while async res is still loading
+        if(this.props.id) {
+            post = <p style={{textAlign:'center'}}>Loading..</p>;
+        }
+
         //check if an id is present
-        if(this.props.id){
+        if(this.state.loadedPost){
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.content}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
